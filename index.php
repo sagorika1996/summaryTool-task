07b70b2779c48b9d84fba195b3b287eca921bdcf
Sagorika Das <sagorika1996@gmail.com>
2017-10-15 19:56:13 +0530

    Added index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>FA count</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script src="https://tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
+    <link rel = "stylesheet" type = "text/css" href = "index.css" />
+  </head>
+
+  <body>
+    <div class="jumbotron text-center">
+      <h1>Featured Article - Micro Task</h1> 
+      <p>Enter username</p> 
+      <form class="form-inline" id="myForm" action="" method="post">
+        <div class="input-group">
+          <input name="searchTerm" type="text" class="form-control" size="50" placeholder="Username" required>
+          <div class="input-group-btn">
+            <button class="btn btn-default" type="submit"> 
+              <i class="glyphicon glyphicon-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
